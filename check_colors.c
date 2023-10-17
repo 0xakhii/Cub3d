c334c0b8108c13214ac6900522c5f57f8b69c04a
Omar Jamal <ojamal@student.1337.ma>
2023-10-17 04:23:40 +0100

    still the same problem trying to fix it

--- check_colors.c
+++ check_colors.c
@@ -6,7 +6,7 @@
 /*   By: ojamal <ojamal@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/14 23:43:17 by ojamal            #+#    #+#             */
-/*   Updated: 2023/10/17 04:11:26 by ojamal           ###   ########.fr       */
+/*   Updated: 2023/10/17 04:23:11 by ojamal           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,8 +35,8 @@ int check_color(char *str)
             return (ft_putstr_fd("\033[1;31mError\nCube3D :\033[0;0m Invalid color format\n", 2), 1);
         else if (ft_isdigit(str[i]) && ft_atoi(&str[i]) >= 0 && ft_atoi(&str[i]) <= 255)
             count++;
-        if ((count > 3 && count > 0) && j > 2)
-            return (ft_putstr_fd("\033[1;31mError\nCube3D :\033[0;0m Invalid color format\n", 2), 1);
+        if (count > 3 || count == 0 || j > 2)
+            return (ft_putstr_fd("\033[1;31mError\nCube3D :\033[0;0m Invalid color format \n", 2), 1);
         i++;
     }
     return (0);
